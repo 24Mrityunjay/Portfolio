aa19da3eca37d26ed5d7329da50f4a3c5272dc63
Mrityunjay Yadav <54mrityunjay@gmail.com>
2026-02-26 23:27:56 +0530

    Updated my projects

--- src/components/pages/portfolio/Portfolio.jsx
+++ src/components/pages/portfolio/Portfolio.jsx
@@ -1,41 +1,67 @@
 import { useState } from "react";
 import './Portfolio.css';
+import portfolio from '../../../assets/portfolio.png';
+import ailandingpage from '../../../assets/landingpage.png';
+import admindashboard from '../../../assets/admindashboard.png';
+import moderntodo from '../../../assets/moderntodo.png';
+import todo from '../../../assets/todo.png';
 
 const projectsData = [
         {
         id: 1,
-        title: "Web design",
+        title: "Responsive Portfolio",
         category: "web",
-        image: "https://cdn.dribbble.com/userupload/17067692/file/original-86095f67ee600a4aecca6cf2465919f6.png?resize=2048x1536&vertical=center",
-        demo: "https://cdn.dribbble.com/userupload/17067692/file/original-86095f67ee600a4aecca6cf2465919f6.png?resize=2048x1536&vertical=center",
+        image: portfolio,
+        demo: "https://mrityunjayy-portfolio.netlify.app/",
     },
     {
         id: 2,
-        title: "Web design",
+        title: "AI Landing Page",
         category: "web",
-        image: "https://cdn.dribbble.com/userupload/14912902/file/original-12c002c69215970cd2eaf4824b287a4f.png?resize=2048x1536&vertical=center",
-        demo: "https://cdn.dribbble.com/userupload/14912902/file/original-12c002c69215970cd2eaf4824b287a4f.png?resize=2048x1536&vertical=center",
+        image: ailandingpage,
+        demo: "https://landing-page-mj.netlify.app/",
     },
     {
         id: 3,
+        title: "Admin dashboard",
+        category: "web",
+        image: admindashboard,
+        demo: "https://admin-dashboard-bymj.netlify.app/",
+    },
+    {
+        id: 4,
+        title: "Modern Todo App",
+        category: "web",
+        image: moderntodo,
+        demo: "https://todo-by-taskly.netlify.app/",
+    },
+     {
+        id: 5,
+        title: "Todo App",
+        category: "web",
+        image: todo,
+        demo: "https://todo-bymj.netlify.app/",
+    },
+    {
+        id: 6,
+        title: "Design",
+        category: "design",
+        image: "https://cdn.dribbble.com/userupload/40443513/file/original-af1bcfb920211ae8de4d2b34f8ad9c69.png?resize=1200x900&vertical=center",
+        demo: "https://cdn.dribbble.com/userupload/40443513/file/original-af1bcfb920211ae8de4d2b34f8ad9c69.png?resize=1200x900&vertical=center",
+    },
+    {
+        id: 7,
         title: "Mobile App design",
         category: "app",
         image: "https://cdn.dribbble.com/userupload/4585415/file/original-7b9b300faf22e9fe2fd176ec1c70cadc.jpg?resize=1504x1128&vertical=center",
         demo: "https://cdn.dribbble.com/userupload/4585415/file/original-7b9b300faf22e9fe2fd176ec1c70cadc.jpg?resize=1504x1128&vertical=center",
     },
     {
-        id: 5,
+        id: 8,
         title: "Mobile App design",
         category: "app",
         image: "https://cdn.dribbble.com/userupload/9141679/file/original-2a3e9ad21b2fa36e4123636da1fdb45b.jpg?resize=2048x1536&vertical=center",
         demo: "https://cdn.dribbble.com/userupload/9141679/file/original-2a3e9ad21b2fa36e4123636da1fdb45b.jpg?resize=2048x1536&vertical=center",
-    },
-    {
-        id: 4,
-        title: "Design",
-        category: "design",
-        image: "https://cdn.dribbble.com/userupload/40443513/file/original-af1bcfb920211ae8de4d2b34f8ad9c69.png?resize=1200x900&vertical=center",
-        demo: "https://cdn.dribbble.com/userupload/40443513/file/original-af1bcfb920211ae8de4d2b34f8ad9c69.png?resize=1200x900&vertical=center",
     },
 ];
 
